741dc95984d6f09fe3dec3b4a458744b77d16dac
Martin Robinson <martinjrobins@gmail.com>
2014-10-16 03:08:56 +0100

    LdG solves theta

--- LdG.py
+++ LdG.py
@@ -8,18 +8,18 @@ Created on Wed Oct 15 14:17:38 2014
 from dolfin import *
 
 # Create mesh
-mesh = UnitSquareMesh(8, 8)
+mesh = UnitSquareMesh(10, 10)
 def bottom(x, on_boundary):
-    return near(x[1],0) and on_boundary
+    return near(x[1],0.0) and on_boundary
 def top(x, on_boundary):
-    return near(x[1],L) and on_boundary
+    return near(x[1],1.0) and on_boundary
 def left(x, on_boundary):
-    return near(x[0],0) and on_boundary
+    return near(x[0],0.0) and on_boundary
 def right(x, on_boundary):
-    return near(x[0],L) and on_boundary
+    return near(x[0],1.0) and on_boundary
 
 # Define function space
-V = FunctionSpace(mesh, "Lagrange", 1)
+V = FunctionSpace(mesh, "CG", 1)
 
 # Define boundary condition
 u_bottom = Constant(0)
@@ -32,39 +32,25 @@ bc_left = DirichletBC(V, u_left, left)
 bc_right = DirichletBC(V, u_right, right)
 bc = [bc_bottom,bc_top,bc_left,bc_right]
 
+
+theta = TrialFunction(V)
+v = TestFunction(V)
+
+# Define variational problem
+a = inner(grad(theta), grad(v))*dx
+L = Constant(0)*v*dx
+
+# Compute solution
+theta = Function(V)
+solve(a == L, theta, bc)
+
+# Plot solution
+plot(theta, interactive=True)
+
+
 # Define variational problem
 #(Q11,Q12) = TrialFunction(V)
 #(v11,v12) = TestFunction(V)
 #
 #eps = Constant(1.0)
 #a = (inner(grad(Q11), grad(v11))*dx() + (2/eps**2)*(Q11*Q11 + Q12*Q12 - 1)*Q11*v11)*dx()
-
-a2 = Constant("triangle")
-c2 =  Constant("triangle")
-
-
-L = f*v*dx() + g*v*ds()
-
-# Define function for the solution
-u = Function(V)
-
-# Define goal functional (quantity of interest)
-M = u*dx()
-
-# Define error tolerance
-tol = 1.e-5
-
-# Solve equation a = L with respect to u and the given boundary
-# conditions, such that the estimated error (measured in M) is less
-# than tol
-problem = LinearVariationalProblem(a, L, u, bc)
-solver = AdaptiveLinearVariationalSolver(problem, M)
-solver.parameters["error_control"]["dual_variational_solver"]["linear_solver"] = "cg"
-solver.solve(tol)
-
-solver.summary()
-
-# Plot solution(s)
-plot(u.root_node(), title="Solution on initial mesh")
-plot(u.leaf_node(), title="Solution on final mesh")
-interactive()
