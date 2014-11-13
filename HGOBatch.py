5c4c31ec5ce272a553b176f86e991b169b16c4b1
martinjrobins <martinjrobins@gmail.com>
2014-11-13 14:23:59 +0000

    changed GBB and HGOB to batch mode

--- HGOBatch.py
+++ HGOBatch.py
@@ -20,93 +20,96 @@ N = int(rho*L**2/sigma_s**2)
 print 'adding ',N,' particles...'
 
 
-N_b = 10**5
+N_b = 10**1
 tau_s = 10**(-4)
 
 averaging_diameter = 2.5
 
-params = Params()
-params['Dtrans'] = diff_step
-params['Drot'] = rot_step
-params['Temp'] = T
-params['h'] = averaging_diameter
-params['L'] = L
 
 assert len(sys.argv)==2
 out_dir = sys.argv[1]
 if not os.path.exists(out_dir):
     os.makedirs(out_dir)
 
-
-particles = Particles()
-for i in range(N):
-    p = Particle()
-    eps = sigma_s
-    p.position = Vect3d(uniform(eps,L-eps),uniform(eps,L-eps),0)
-    p.theta = uniform(0,2*pi)
-    p.fixed = False
-    particles.append(p)
+def run_simulation(run):
+    print 'doing run %d...'%run
     
-sigma_e = sigma_s*k
-n_side = int(L/sigma_e)
-spacing = L/n_side
-for i in range(n_side):
-    p = Particle()
-    p.position = Vect3d(0,(i+0.5)*spacing,0)
-    p.theta = pi/2
-    p.fixed = True
-    particles.append(p)
+    params = Params()
+    params['Dtrans'] = diff_step
+    params['Drot'] = rot_step
+    params['Temp'] = T
+    params['h'] = averaging_diameter
+    params['L'] = L
     
-    p = Particle()
-    p.position = Vect3d(L,(i+0.5)*spacing,0)
-    p.theta = pi/2
-    p.fixed = True
-    particles.append(p)
-    
-    p = Particle()
-    p.position = Vect3d((i+0.5)*spacing,0,0)
-    p.theta = 0
-    p.fixed = True
-    particles.append(p)
-    
-    p = Particle()
-    p.position = Vect3d((i+0.5)*spacing,L,0)
-    p.theta = 0
-    p.fixed = True
-    particles.append(p)
-    
-lattice_particles = Particles()
-N = int(L)
-for i in range(N+1):
-    for j in range(N+1):
+    particles = Particles()
+    for i in range(N):
         p = Particle()
-        p.position = Vect3d(i,j,0)
+        eps = sigma_s
+        p.position = Vect3d(uniform(eps,L-eps),uniform(eps,L-eps),0)
+        p.theta = uniform(0,2*pi)
+        p.fixed = False
+        particles.append(p)
+        
+    sigma_e = sigma_s*k
+    n_side = int(L/sigma_e)
+    spacing = L/n_side
+    for i in range(n_side):
+        p = Particle()
+        p.position = Vect3d(0,(i+0.5)*spacing,0)
+        p.theta = pi/2
         p.fixed = True
-        lattice_particles.append(p)
- 
-U_hgo = HGOPotential(sigma_s=sigma_s,k=k)
-
-tau = monte_carlo_timestep(N_b,0,particles,lattice_particles,U_hgo,params)[2]
-
-run = 0
-f = open('%s/U%04d'%(out_dir,run), 'w')
-
-params['Dtrans'] = diff_step
-params['Drot'] = rot_step
-
-for batch in range(200):
-    tau = monte_carlo_timestep(N_b,N_b/10,particles,lattice_particles,U_hgo,params)[2]
-    print tau
+        particles.append(p)
+        
+        p = Particle()
+        p.position = Vect3d(L,(i+0.5)*spacing,0)
+        p.theta = pi/2
+        p.fixed = True
+        particles.append(p)
+        
+        p = Particle()
+        p.position = Vect3d((i+0.5)*spacing,0,0)
+        p.theta = 0
+        p.fixed = True
+        particles.append(p)
+        
+        p = Particle()
+        p.position = Vect3d((i+0.5)*spacing,L,0)
+        p.theta = 0
+        p.fixed = True
+        particles.append(p)
+        
+    lattice_particles = Particles()
+    N = int(L)
+    for i in range(N+1):N_b/10
+        for j in range(N+1):
+            p = Particle()
+            p.position = Vect3d(i,j,0)
+            p.fixed = True
+            lattice_particles.append(p)
+     
+    U_hgo = HGOPotential(sigma_s=sigma_s,k=k)
     
-    f.write('%d %f\n'%(batch,tau))
-    f.flush()
+    f = open('%s/U%04d'%(out_dir,run), 'w')
+
+    for batch in range(50):
+        tau = monte_carlo_timestep(N_b,0,particles,lattice_particles,U_hgo,params)
+        s = sqrt(tau[0]**2+tau[1]**2)
+        print 's = ',s,' U = ',tau[2]
+        f.write('%f %f\n'%(s,tau[2]))
+        f.flush()
     
-    w = tvtk.XMLUnstructuredGridWriter(input=particles.get_grid(), file_name='%s/vtkBatch%04d.vtu'%(out_dir,batch))
+    tau = monte_carlo_timestep(N_b*10,N_b,particles,lattice_particles,U_hgo,params)
+    s = sqrt(tau[0]**2+tau[1]**2)
+    print 's = ',s,' U = ',tau[2]
+    f.write('%f %f\n'%(s,tau[2]))
+    f.close()
+        
+    w = tvtk.XMLUnstructuredGridWriter(input=particles.get_grid(), file_name='%s/finalBatch%04d.vtu'%(out_dir,run))
     w.write()
 
-    w = tvtk.XMLUnstructuredGridWriter(input=lattice_particles.get_grid(), file_name='%s/vtkAveraged%04d.vtu'%(out_dir,batch))
+    w = tvtk.XMLUnstructuredGridWriter(input=lattice_particles.get_grid(), file_name='%s/finalAveraged%04d.vtu'%(out_dir,run))
     w.write()
+        
     
-
-f.close()
-  
+pool = Pool(processes=2)
+pool.map(run_simulation, range(100))  
